604b44b30c1f1b8934c1c73d4eec580d08864381
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-08-20 20:07:40 +0330

    create new template and try to ffix it

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,11 @@
 from website.views import *
 from django.urls import path,include
 
+app_name = "website"
 
 urlpatterns = [
-    path('',index_view),
-    path('about',about_view),
+    path('',index_view,name='index'),
+    path('about',about_view,name='about'),
     path('contact',contact_view),
     path('login',login_view),
     path('signup',signup_view),
