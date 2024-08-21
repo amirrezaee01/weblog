7d9f7d79ded05990e68f9153f6ec725107256df7
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-08-21 17:57:27 +0330

    create a model for website and blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    #image
+    #author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    #tag
+    #category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return "{} - {}".format(self.title,self.id)
