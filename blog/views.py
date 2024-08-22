9e69880b1d67e08eddaf83a45f38be39400df46c
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-08-22 18:52:33 +0330

    added next and pervious func to blog single

--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,24 @@ def blog_view(request):
     
 def blog_single(request,pid):
     now = timezone.now()
+
     post = get_object_or_404(Post, published_date__lte=now, pk=pid, status=1)
     post.counted_views += 1
     post.save()
-    context = {'post':post}
+
+    posts = Post.objects.filter(status=1, published_date__lte=now).order_by('published_date')
+    
+    post_ids = list(posts.values_list('id', flat=True))
+    current_index = post_ids.index(post.id)
+
+    prev_post = posts[current_index - 1] if current_index > 0 else None
+    next_post = posts[current_index + 1] if current_index < len(post_ids) - 1 else None
+    
+    context = {
+        'post': post,
+        'prev_post': prev_post,
+        'next_post': next_post,
+    }
     return render(request,'blog/blog-single.html',context)
     
 
