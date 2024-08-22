2abb8c9c117d3691a97d6fca0f0e18abe1eb4a66
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-08-22 12:04:43 +0330

    update code
    i add func counteed views and get tha data post from database and show it in the tempate

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
+from django.utils import timezone
 # Create your views here.
 
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
+    now = timezone.now()
+    posts = Post.objects.filter(published_date__lte=now,status=1)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
     
-def blog_single(request):
-    return render(request,'blog/blog-single.html')
+def blog_single(request,pid):
+    now = timezone.now()
+    post = get_object_or_404(Post, published_date__lte=now, pk=pid, status=1)
+    post.counted_views += 1
+    post.save()
+    context = {'post':post}
+    return render(request,'blog/blog-single.html',context)
     
 
