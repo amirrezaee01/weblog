0f36891acdd753f23e80f309cd3486ccb7e9d85a
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-08-25 13:12:38 +0330

    show how much category we have

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post,Category
 
 
 register = template.Library()
@@ -9,3 +9,14 @@ def latestpost():
     posts = Post.objects.filter(status=1).order_by('-published_date')[:3]
     return {'posts':posts}
 
+@register.inclusion_tag('blog/blog-category.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        post_count = posts.filter(category=name).count()
+        
+        cat_dict[name] = post_count
+    
+    return {'categories': cat_dict}
