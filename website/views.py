ee3e00f22210b953012eb3d9072459af47ebabf7
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-08-27 00:43:23 +0330

    make the contact page compelete

--- website/views.py
+++ website/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from website.forms import ContactForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 def index_view(request):
-    return render(request,'website/index.html')
+    return render(request, 'website/index.html')
 
 def about_view(request):
-    return render(request,'website/about.html')
-    
-    
+    return render(request, 'website/about.html')
+
 def contact_view(request):
-    return render(request,'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your ticket was submitted correctly.')
+        else:
+            messages.add_message(request, messages.ERROR, 'Your ticket did not submit correctly.')
+    else:
+        form = ContactForm()
+
+    return render(request, 'website/contact.html', {'form': form})
 
 def login_view(request):
     return render(request,'accounts/login.html')
