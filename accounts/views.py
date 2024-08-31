e87cc467dc6af85097e15034ff705999fae1b1a7
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-09-01 00:20:50 +0330

    added signup to project

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login,logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.contrib import messages
+
 
 
 # Create your views here.
@@ -29,6 +32,20 @@ def logout_view(request):
     logout(request) 
     return redirect('/')
     
-
 def signup_view(request):
-    return render(request,'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Account created successfully!')
+                return redirect('accounts:login')
+            else:
+                messages.error(request, 'Please correct the errors below.')
+        else:
+            form = UserCreationForm()
+        
+        context = {'form': form}
+        return render(request, 'accounts/signup.html', context)
+    else:
+        return redirect('/')
