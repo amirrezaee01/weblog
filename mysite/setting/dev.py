92387547d3636abcd112fd553800893db2961c8e
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2024-09-15 18:49:12 +0330

    add django compressor and httpacceess
    do not forget to add these things in your host to

--- mysite/setting/dev.py
+++ mysite/setting/dev.py
@@ -1,22 +1,11 @@
 from mysite.settings import *
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Development-specific settings
 SECRET_KEY = 'django-insecure-my^xk1b6@oj#bj!p0y5&lul1q%ai$%7wqi5mx&4@#7j5)ehc@+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
-#INSTALLED_APPS = []
-#site id
 SITE_ID = 2
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -25,11 +14,13 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / "statics"]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
+# Django Compressor settings for development
+COMPRESS_ENABLED = False
+COMPRESS_URL = STATIC_URL
+COMPRESS_ROOT = STATIC_ROOT
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
--- mysite/setting/prod.py
+++ mysite/setting/prod.py
@@ -1,19 +1,11 @@
 from mysite.settings import *
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# Production-specific settings
 SECRET_KEY = 'django-insecure-my^xk1b6@oj#bj!p0y5&lul1q%ai$%7wqi5mx&4@#7j5)ehc@+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS = ["amirrezaee.ir", "www.amirrezaee.ir"]
 SITE_ID = 2
-
-ALLOWED_HOSTS = ["amirrezaee.ir","www.amirrezaee.ir"]
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -24,25 +16,22 @@ DATABASES = {
 
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / "statics"]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
+# Enable compression in production
+COMPRESS_ENABLED = True
+COMPRESS_URL = STATIC_URL
+COMPRESS_ROOT = STATIC_ROOT
 
+# Security settings
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = "SAMEORIGIN"
 SECURE_CONTENT_TYPE_NOSNIFF = True
-## Strict-Transport-Security
 SECURE_HSTS_SECONDS = 15768000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
-
-## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
-SECURE_SSL_REDIRECT = False 
-
-# for more security
-CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = False  # Configure in web server
 CSRF_USE_SESSIONS = True
 CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = True
